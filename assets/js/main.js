4916fe37fc53db096531fc0770ebb2542479eba4
Admin <Admin@CS8VZD3-REW.inc.bytedance.com>
2021-08-26 08:57:44 -0700

    Add mock user auth, payment process, and corresponding event tracking

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
-if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', readyEventHandler)
 } else {
-    ready()
+    readyEventHandler()
 
     ttq.track('ViewContent', {
         content_id: generateRandomID(),
@@ -10,59 +10,167 @@ if (document.readyState == 'loading') {
     });
 }
 
-function checkForPixel() {
-    return ttq ? pixelLoaded = true : pixelLoaded = false
-}
+let userEmailAddress = "";
+let loggedIn = false;
+let external_id = 0;
 
-function ready() {
-    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
-    for (var i = 0; i < removeCartItemButtons.length; i++) {
-        var button = removeCartItemButtons[i]
+function readyEventHandler() {
+    let removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    for (let i = 0; i < removeCartItemButtons.length; i++) {
+        let button = removeCartItemButtons[i]
         button.addEventListener('click', removeCartItem)
     }
 
-    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
-    for (var i = 0; i < quantityInputs.length; i++) {
-        var input = quantityInputs[i]
+    let quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for (let i = 0; i < quantityInputs.length; i++) {
+        let input = quantityInputs[i]
         input.addEventListener('change', quantityChanged)
     }
 
-    var addToCartButtons = document.getElementsByClassName('shop-item-button')
-    for (var i = 0; i < addToCartButtons.length; i++) {
-        var button = addToCartButtons[i]
+    let addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for (let i = 0; i < addToCartButtons.length; i++) {
+        let button = addToCartButtons[i]
         button.addEventListener('click', addToCartClicked)
     }
 
     document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
     document.getElementsByClassName('subscribe')[0].addEventListener('click', subscribeClicked)
-    document.getElementsByClassName('footer__contact')[0].addEventListener('click', pixelTrackSubscribe)
+    document.getElementsByClassName('footer__contact')[0].addEventListener('click', pixelTrackContact)
+    document.getElementsByClassName('login__link')[0].addEventListener('click', mockLogin)
+}
+
+function checkForPixel() {
+    return ttq ? pixelLoaded = true : pixelLoaded = false
+}
+
+function mockLogin() {
+    let emailAddress = prompt("Mock login screen - enter an email address to continue. Entered email address will be used for advanced tracking", "");
+    
+    if (emailAddress.length > 0) {
+      document.getElementById("login").innerHTML = "Welcome " + emailAddress;
+      
+      //Log in user and assign email address and external id values for advanced matching on user for associated events
+      loggedIn = true;
+      userEmailAddress = emailAddress;
+      external_id = generateRandomID();
+    }
+}
+
+function pixelIdentifyHandler(external_id, userEmailAddress) {
+    ttq.identify({
+        external_id: external_id,
+        email: userEmailAddress,
+        phone_number: '+19171234567',
+    })
+}
+
+function mockPaymentDetailsDialogBox() {
+    let retVal = confirm("This is a Mock Payment Information Screen - User Inputs Payment Information here and InitiateCheckout event is triggered. Events CompletePayment " +
+        "and PlaceAnOrder are triggered afterwards. Clicking cancel here stops payment process and only InitiateCheckout event is tracked");
+
+    let cartItemContainer = document.getElementsByClassName('cart-items')[0];
+    let cartRows = cartItemContainer.getElementsByClassName('cart-row');
+
+    let total = 0
+    for (let i = 0; i < cartRows.length; i++) {
+        let cartRow = cartRows[i]
+        let priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        let price = parseFloat(priceElement.innerText.replace('$', ''))
+        let quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+
+    if (loggedIn) {
+        pixelIdentifyHandler(external_id, userEmailAddress)
+    }
+
+    ttq.track('InitiateCheckout', {
+        content_id: generateRandomID(),
+        content_type: 'product',
+        content_name: 'initiate checkout',
+        quantity: parseInt(quantity),
+        price: total,
+        value: total,
+        currency: 'USD',
+    });
+
+    if (retVal === true) {
+        if (!loggedIn) {
+            let emailAddress = prompt("Email address prompt for mock payment and to apply Advanced Matching with related events", "");
+            userEmailAddress = emailAddress;
+
+            document.getElementById("login").innerHTML = "Welcome " + emailAddress;
+        }
+        return true;
+    } else {
+        alert("Payment step cancelled!");
+        return false;
+    }
 }
 
 function purchaseClicked() {
-    alert('Thank you for your purchase')
+    let cartItemContainer = document.getElementsByClassName('cart-items')[0];
+    let cartRows = cartItemContainer.getElementsByClassName('cart-row');
 
-    pixelTrackPurchase();
-
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    while (cartItems.hasChildNodes()) {
-        cartItems.removeChild(cartItems.firstChild)
+    let total = 0
+    for (let i = 0; i < cartRows.length; i++) {
+        let cartRow = cartRows[i]
+        let priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        let price = parseFloat(priceElement.innerText.replace('$', ''))
+        let quantity = quantityElement.value
+        total = total + (price * quantity)
     }
-    updateCartTotal()
+
+    if (total === 0) {
+        alert ("Please add items to your cart!")
+        return;
+    }
+
+    let paymentDetailsEntered = false;
+    paymentDetailsEntered = mockPaymentDetailsDialogBox();
+
+    if (paymentDetailsEntered) {
+        alert('Thank you for your purchase')
+
+        pixelTrackPurchase();
+    
+        let cartItems = document.getElementsByClassName('cart-items')[0]
+        while (cartItems.hasChildNodes()) {
+            cartItems.removeChild(cartItems.firstChild)
+        }
+        updateCartTotal()
+    }
 }
 
 function subscribeClicked() {
-    alert('Thank you for subscribing');
-    pixelTrackSubscribe();
+    if (document.getElementsByClassName("newsletter__input").value === undefined) {
+        alert("Please enter your email address");
+    } else {
+        if (loggedIn) {
+            pixelIdentifyHandler(external_id, userEmailAddress)
+        }
+
+        alert('Thank you for subscribing');
+        pixelTrackSubscribe();
+    }
 }
 
 function removeCartItem(event) {
-    var buttonClicked = event.target
+    let buttonClicked = event.target
     buttonClicked.parentElement.parentElement.remove()
+
+    if (loggedIn) {
+        pixelIdentifyHandler(external_id, userEmailAddress)
+    }
+
+    ttq.track('ClickButton', {content_name: 'clicked remove cart item'});
     updateCartTotal()
 }
 
 function quantityChanged(event) {
-    var input = event.target
+    let input = event.target
     if (isNaN(input.value) || input.value <= 0) {
         input.value = 1
     }
@@ -79,6 +187,10 @@ function pixelTrackAddToCart(title, price) {
     let priceString = price.replace("$", "");
     let priceInt = parseInt(priceString);
 
+    if (loggedIn) {
+        pixelIdentifyHandler(external_id, userEmailAddress)
+    }
+
     ttq.track('AddToCart', {
         content_id: generateRandomID(),
         content_type: 'product',
@@ -93,11 +205,19 @@ function pixelTrackAddToCart(title, price) {
 }
 
 function pixelTrackSubscribe() {
+    if (loggedIn) {
+        pixelIdentifyHandler(external_id, userEmailAddress)
+    }
+
     ttq.track('Subscribe');
     console.log("in the pixelTrackSubscribe");
 }
 
 function pixelTrackContact() {
+    if (loggedIn) {
+        pixelIdentifyHandler(external_id, userEmailAddress)
+    }
+
     ttq.track('Contact', {
         content_id: generateRandomID(),
         content_type: 'product',
@@ -106,31 +226,25 @@ function pixelTrackContact() {
 }
 
 function pixelTrackPurchase() {
-    var cartItemContainer = document.getElementsByClassName('cart-items')[0];
-    var cartRows = cartItemContainer.getElementsByClassName('cart-row');
+    let cartItemContainer = document.getElementsByClassName('cart-items')[0];
+    let cartRows = cartItemContainer.getElementsByClassName('cart-row');
 
-    var total = 0
-    for (var i = 0; i < cartRows.length; i++) {
-        var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-        var price = parseFloat(priceElement.innerText.replace('$', ''))
-        var quantity = quantityElement.value
+    let total = 0
+    for (let i = 0; i < cartRows.length; i++) {
+        let cartRow = cartRows[i]
+        let priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        let price = parseFloat(priceElement.innerText.replace('$', ''))
+        let quantity = quantityElement.value
         total = total + (price * quantity)
     }
 
     let transactionID = generateRandomID();
 
-    ttq.track('InitiateCheckout', {
-        content_id: transactionID,
-        content_type: 'product',
-        content_name: 'initiate checkout',
-        quantity: parseInt(quantity),
-        price: total,
-        value: total,
-        currency: 'USD',
-    });
-
+    if (loggedIn) {
+        pixelIdentifyHandler(external_id, userEmailAddress)
+    }
+    
     ttq.track('CompletePayment', {
         content_id: transactionID,
         content_type: 'product',
@@ -155,28 +269,28 @@ function pixelTrackPurchase() {
 }
 
 function addToCartClicked(event) {
-    var button = event.target
-    var shopItem = button.parentElement.parentElement
-    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
-    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
-    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    let button = event.target
+    let shopItem = button.parentElement.parentElement
+    let title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    let price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    let imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
     addItemToCart(title, price, imageSrc)
     pixelTrackAddToCart(title, price);
     updateCartTotal()
 }
 
 function addItemToCart(title, price, imageSrc) {
-    var cartRow = document.createElement('div')
+    let cartRow = document.createElement('div')
     cartRow.classList.add('cart-row')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
-    for (var i = 0; i < cartItemNames.length; i++) {
-        if (cartItemNames[i].innerText == title) {
+    let cartItems = document.getElementsByClassName('cart-items')[0]
+    let cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for (let i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText === title) {
             alert('This item is already added to the cart')
             return
         }
     }
-    var cartRowContents = `
+    let cartRowContents = `
         <div class="cart-item cart-column">
             <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
             <span class="cart-item-title">${title}</span>
@@ -193,15 +307,15 @@ function addItemToCart(title, price, imageSrc) {
 }
 
 function updateCartTotal() {
-    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
-    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
-    var total = 0
-    for (var i = 0; i < cartRows.length; i++) {
-        var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-        var price = parseFloat(priceElement.innerText.replace('$', ''))
-        var quantity = quantityElement.value
+    let cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    let cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    let total = 0
+    for (let i = 0; i < cartRows.length; i++) {
+        let cartRow = cartRows[i]
+        let priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        let price = parseFloat(priceElement.innerText.replace('$', ''))
+        let quantity = quantityElement.value
         total = total + (price * quantity)
     }
     total = Math.round(total * 100) / 100
